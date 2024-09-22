40a5990e5b7f3e5340cd5bd06b07814ff967343b
tony007x <aekanan2004@gmail.com>
2024-09-22 17:44:18 +0700

    shadcn and lucidICON

---
+++ src/routes/home/(component)/Nav.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Skull } from "lucide-svelte";
+</script>
+
+<div class="flex w-full h-[70px] border border-gray-800 m-10 rounded-full items-center p-4 text-white justify-around bg-black">
+    <div class="flex w-full gap-3 items-center">
+        <Skull color="white"/>
+        <p>TONY219Y</p>
+    </div>
+    <div  class="flex w-full border">
+        <ul class="flex w-full justify-between border">
+            <li>Services</li>
+            <li>Portfilio</li>
+            <li>Talk to me</li>
+        </ul>
+
+    </div>
+    <div  class="flex w-full border">
+        <h1>hello</h1>
+    </div>
+
+</div>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Nav from "./(component)/Nav.svelte";
+</script>
+
+<div class="flex w-full h-screen" >
+    <Nav/>
+</div>
+
+
+
